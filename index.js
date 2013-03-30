bfadb697a63b387f720c9a60923377ecefd97184
Kristoffer Walker <kris@kixx.name>
2013-03-30 00:26:29 -0400

    Close #1: Pass path objects into path constructor
     On branch master
    
    	modified:   index.js
    	modified:   test.js

--- index.js
+++ index.js
@@ -8,82 +8,87 @@ var FS = require('fs')
 
 
 exports.newPath = function newPath(path) {
-    var self = Object.create(null)
+  var self = Object.create(null)
+    , args = slice.call(arguments).map(function (item) {
+      return item +'';
+    })
 
-    if (arguments.length > 1) {
-        path = PATH.join.apply(PATH, arguments);
-    }
+  if (args.length > 1) {
+    path = PATH.join.apply(PATH, args);
+  } else {
+    path = args[0];
+  }
 
-    self.append = function append() {
-        // Join an arbitrary number of arguments.
-        return newPath.apply(null, [path].concat(slice.call(arguments)));
-    };
+  self.append = function append() {
+    // Join an arbitrary number of arguments.
+    return newPath.apply(null, [path].concat(slice.call(arguments)));
+  };
 
-    self.exists = function exists() {
-        return FS.existsSync(path) ? true : false;
-    };
+  self.exists = function exists() {
+    return FS.existsSync(path) ? true : false;
+  };
 
-    self.read = function read(opts) {
-        var d = IOU.newDefer()
+  self.read = function read(opts) {
+    var d = IOU.newDefer()
 
-        // Break opts apart to make it immutable
-        opts || (opts = {});
-        encoding = opts.encoding || 'utf8';
-        parser = opts.parser;
+    // Break opts apart to make it immutable
+    opts || (opts = {});
+    encoding = opts.encoding || 'utf8';
+    parser = opts.parser;
 
-        FS.readFile(path, encoding, function (err, data) {
-            var msg, e
+    FS.readFile(path, encoding, function (err, data) {
+      var msg, e
 
-            if (err && err.code === 'ENOENT') {
-                return d.keep(null);
-            } else if (err && err.code === 'EISDIR') {
-                e = new Error("Cannot read '"+ path +"'; it is a directory.");
-                e.code = "path is directory";
-                return d.fail(e);
-            }
+      if (err && err.code === 'ENOENT') {
+        return d.keep(null);
+      } else if (err && err.code === 'EISDIR') {
+        e = new Error("Cannot read '"+ path +"'; it is a directory.");
+        e.code = "path is directory";
+        return d.fail(e);
+      }
 
-            // If a parser is specified, use it to deserialize the text.
-            switch (parser) {
-            case 'ini':
-                try {
-                    return d.keep(decodeINI(data));
-                } catch (iniErr) {
-                    return d.fail(iniErr);
-                }
-            case 'JSON':
-                try {
-                    return d.keep(decodeJSON(data));
-                } catch (jsonErr) {
-                    return d.fail(jsonErr);
-                }
-            default:
-                return d.keep(data);
-            }
-        });
+      // If a parser is specified, use it to deserialize the text.
+      switch (parser) {
+      case 'ini':
+        try {
+          return d.keep(decodeINI(data));
+        } catch (iniErr) {
+          return d.fail(iniErr);
+        }
+      case 'JSON':
+        try {
+          return d.keep(decodeJSON(data));
+        } catch (jsonErr) {
+          return d.fail(jsonErr);
+        }
+      default:
+        return d.keep(data);
+      }
+    });
 
-        return d.promise;
-    };
+    return d.promise;
+  };
 
-    self.home = function home() {
-        // This module is not really Windows ready, but this is how it might be
-        // done.
-        return newPath(process.platform === 'win32' ?
-            process.env.USERPROFILE : process.env.HOME);
-    };
+  self.home = function home() {
+    // This module is not really Windows ready, but this is how it might be
+    // done.
+    return newPath(process.platform === 'win32' ?
+      process.env.USERPROFILE : process.env.HOME);
+  };
 
-    self.toString = function toString() {
-        return path;
-    };
+  self.toString = function toString() {
+    return path;
+  };
 
-    return self;
+  return self;
 };
 
 
 function decodeINI(str) {
-    return INI.decode(str);
+  return INI.decode(str);
 }
 
 
 function decodeJSON(str) {
-    return JSON.parse(str);
+  return JSON.parse(str);
 }
--- test.js
+++ test.js
@@ -5,211 +5,220 @@ var tests = [];
 
 
 tests.push(function (done) {
-    // Create a new file path.
+  // Create a new file path.
 
-    var path = FILEPATH.newPath('foo');
+  var path = FILEPATH.newPath('foo');
 
-    equal(path.toString(), 'foo', '.toString()');
+  equal(path.toString(), 'foo', '.toString()');
 
-    // Multiple path parts.
-    path = FILEPATH.newPath('foo', 'bar', 'baz');
-    equal(path.toString(), 'foo/bar/baz', '.newPath(foo, bar, baz)');
-    return done();
+  // Multiple path parts.
+  path = FILEPATH.newPath('foo', 'bar', 'baz');
+  equal(path.toString(), 'foo/bar/baz', '.newPath(foo, bar, baz)');
+
+  // With a path object.
+  path = FILEPATH.newPath(FILEPATH.newPath('foo'));
+  equal(path.toString(), 'foo', 'path object .toString()');
+
+  // Multiple path objects.
+  path = FILEPATH.newPath(FILEPATH.newPath('foo'), 'bar', FILEPATH.newPath('baz'));
+  equal(path.toString(), 'foo/bar/baz', 'path objects .newPath(foo, bar, baz)');
+
+  return done();
 });
 
 tests.push(function (done) {
-    // .append() method.
+  // .append() method.
 
-    var p1 = FILEPATH.newPath('foo');
+  var p1 = FILEPATH.newPath('foo');
 
-    var p2 = p1.append('bar');
-    // Internal state is not changed.
-    equal(p1.toString(), 'foo', '.append(bar)');
+  var p2 = p1.append('bar');
+  // Internal state is not changed.
+  equal(p1.toString(), 'foo', '.append(bar)');
 
-    // A new path object has been created.
-    equal(p2.toString(), 'foo/bar', 'new .append(bar)');
+  // A new path object has been created.
+  equal(p2.toString(), 'foo/bar', 'new .append(bar)');
 
-    // Append multiple path parts.
-    var p3 = p1.append('bar', 'baz');
-    equal(p3.toString(), 'foo/bar/baz', '.append(bar, baz)');
+  // Append multiple path parts.
+  var p3 = p1.append('bar', 'baz');
+  equal(p3.toString(), 'foo/bar/baz', '.append(bar, baz)');
 
-    return done();
+  return done();
 });
 
 tests.push(function (done) {
-    // .exists() method.
+  // .exists() method.
 
-    var path = FILEPATH.newPath('foo');
-    equal(path.exists(), false, 'exists() === false');
+  var path = FILEPATH.newPath('foo');
+  equal(path.exists(), false, 'exists() === false');
 
-    path = FILEPATH.newPath(__filename);
-    equal(path.exists(), true, 'exists() === true');
+  path = FILEPATH.newPath(__filename);
+  equal(path.exists(), true, 'exists() === true');
 
-    return done();
+  return done();
 });
 
 tests.push(function (done) {
-    // .read() method - not found.
+  // .read() method - not found.
 
-    var path = FILEPATH.newPath('foo')
+  var path = FILEPATH.newPath('foo')
 
-    function onNotFound(rv) {
-        equal(rv, null, 'not found rv === null');
-    }
+  function onNotFound(rv) {
+    equal(rv, null, 'not found rv === null');
+  }
 
-    function onFailure(err) {
-        console.error(err.stack);
-        process.exit(1);
-    }
+  function onFailure(err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
 
-    path.read()
-        .then(onNotFound)
-        .failure(onFailure)
-        .then(done)
-        .failure(fail)
+  path.read()
+    .then(onNotFound)
+    .failure(onFailure)
+    .then(done)
+    .failure(fail)
 });
 
 tests.push(function (done) {
-    // .read() method - is directory.
+  // .read() method - is directory.
 
-    var path = FILEPATH.newPath(__dirname)
+  var path = FILEPATH.newPath(__dirname)
 
-    function skip(rv) {
-        console.log(rv);
-        assert(false, 'should not be called');
-    }
+  function skip(rv) {
+    console.log(rv);
+    assert(false, 'should not be called');
+  }
 
-    function onFailure(err) {
-        equal(err.code, "path is directory", 'Error code');
-        equal(err.message, "Cannot read '"+ __dirname +"'; it is a directory.", 'Error message');
-    }
+  function onFailure(err) {
+    equal(err.code, "path is directory", 'Error code');
+    equal(err.message, "Cannot read '"+ __dirname +"'; it is a directory.", 'Error message');
+  }
 
-    path.read()
-        .then(skip)
-        .failure(onFailure)
-        .failure(function (err) {
-            console.error(err, err.stack);
-            process.exit(1);
-        })
-        .then(done)
-        .failure(fail)
+  path.read()
+    .then(skip)
+    .failure(onFailure)
+    .failure(function (err) {
+      console.error(err, err.stack);
+      process.exit(1);
+    })
+    .then(done)
+    .failure(fail)
 });
 
 tests.push(function (done) {
-    // .read() method - plain text.
+  // .read() method - plain text.
 
-    var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
+  var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
 
-    function testPlainText(rv) {
-        equal(rv, 'foo=bar\n', 'plain text');
-    }
+  function testPlainText(rv) {
+    equal(rv, 'foo=bar\n', 'plain text');
+  }
 
-    function onFailure(err) {
-        console.error(err.stack);
-        process.exit(1);
-    }
+  function onFailure(err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
 
-    path.read()
-        .then(testPlainText)
-        .failure(onFailure)
-        .then(done)
-        .failure(fail)
+  path.read()
+    .then(testPlainText)
+    .failure(onFailure)
+    .then(done)
+    .failure(fail)
 });
 
 tests.push(function (done) {
-    // .read() method - JSON.
+  // .read() method - JSON.
 
-    var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.json')
+  var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.json')
 
-    function testJSON(rv) {
-        equal(rv.foo, 'bar', 'JSON');
-    }
+  function testJSON(rv) {
+    equal(rv.foo, 'bar', 'JSON');
+  }
 
-    function onFailure(err) {
-        console.error(err.stack);
-        process.exit(1);
-    }
+  function onFailure(err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
 
-    path.read({parser: 'JSON'})
-        .then(testJSON)
-        .failure(onFailure)
-        .then(done)
-        .failure(fail)
+  path.read({parser: 'JSON'})
+    .then(testJSON)
+    .failure(onFailure)
+    .then(done)
+    .failure(fail)
 });
 
 tests.push(function (done) {
-    // .read() method - JSON w/error.
+  // .read() method - JSON w/error.
 
-    var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
+  var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
 
-    function skip(rv) {
-        assert(false, 'should not be called');
-    }
+  function skip(rv) {
+    assert(false, 'should not be called');
+  }
 
-    function onFailure(err) {
-        equal(err.name, 'SyntaxError');
-    }
+  function onFailure(err) {
+    equal(err.name, 'SyntaxError');
+  }
 
-    path.read({parser: 'JSON'})
-        .then(skip)
-        .failure(onFailure)
-        .then(done)
-        .failure(fail)
+  path.read({parser: 'JSON'})
+    .then(skip)
+    .failure(onFailure)
+    .then(done)
+    .failure(fail)
 });
 
 tests.push(function (done) {
-    // .read() method - ini.
+  // .read() method - ini.
 
-    var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
+  var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
 
-    function testIni(rv) {
-        equal(rv.foo, 'bar', 'ini');
-    }
+  function testIni(rv) {
+    equal(rv.foo, 'bar', 'ini');
+  }
 
-    function onFailure(err) {
-        console.error(err.stack);
-        process.exit(1);
-    }
+  function onFailure(err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
 
-    path.read({parser: 'ini'})
-        .then(testIni)
-        .failure(onFailure)
-        .then(done)
-        .failure(fail)
+  path.read({parser: 'ini'})
+    .then(testIni)
+    .failure(onFailure)
+    .then(done)
+    .failure(fail)
 });
 
 // Can't seem to create an ini syntax error.
 /*
 tests.push(function (done) {
-    // .read() method - ini w/error.
+  // .read() method - ini w/error.
 
-    var path = FILEPATH.newPath(__dirname, 'fixtures', 'error.ini')
+  var path = FILEPATH.newPath(__dirname, 'fixtures', 'error.ini')
 
-    function skip(rv) {
-        assert(false, 'should not be called');
-        return done();
-    }
+  function skip(rv) {
+    assert(false, 'should not be called');
+    return done();
+  }
 
-    function onFailure(err) {
-        console.error(err.stack);
-    }
+  function onFailure(err) {
+    console.error(err.stack);
+  }
 
-    path.read({parser: 'ini'})
-        .then(skip)
-        .failure(onFailure)
-        .then(done)
+  path.read({parser: 'ini'})
+    .then(skip)
+    .failure(onFailure)
+    .then(done)
 });
 */
 
 tests.push(function (done) {
-    // .home() method.
+  // .home() method.
 
-    var p1 = FILEPATH.newPath(__filename)
-      , p2 = p1.home()
+  var p1 = FILEPATH.newPath(__filename)
+    , p2 = p1.home()
 
-    notEqual(p1, p2, "p1 isnt p2");
-    equal(p2.toString(), process.env.HOME, "home dir");
-    return done();
+  notEqual(p1, p2, "p1 isnt p2");
+  equal(p2.toString(), process.env.HOME, "home dir");
+  return done();
 });
 
 
@@ -223,40 +232,40 @@ tests.push(function () { console.log('PASSED'); });
 // Test vocabulary
 
 function assert(val, msg) {
-    msg = 'assert '+ val +'; '+ msg;
-    return ASSERT.ok(val, msg);
+  msg = 'assert '+ val +'; '+ msg;
+  return ASSERT.ok(val, msg);
 }
 
 function equal(actual, expected, msg) {
-    msg = actual +' !== '+ expected +'; '+ msg;
-    return ASSERT.strictEqual(actual, expected, msg);
+  msg = actual +' !== '+ expected +'; '+ msg;
+  return ASSERT.strictEqual(actual, expected, msg);
 }
 
 function notEqual(actual, expected, msg) {
-    msg = actual +' == '+ expected +'; '+ msg;
-    return ASSERT.notEqual(actual, expected, msg);
+  msg = actual +' == '+ expected +'; '+ msg;
+  return ASSERT.notEqual(actual, expected, msg);
 }
 
 function fail(e) {
-    console.log(e.stack);
-    process.exit(1);
+  console.log(e.stack);
+  process.exit(1);
 }
 
 // Compose test functions using continuation passing.
 function asyncStack(functions) {
-    var callback = functions.pop()
-      , last = callback
+  var callback = functions.pop()
+    , last = callback
 
-    functions.reverse().forEach(function (fn) {
-        var child = callback;
-        callback = function () {
-            fn(child);
-        };
-    });
-    return callback;
+  functions.reverse().forEach(function (fn) {
+    var child = callback;
+    callback = function () {
+      fn(child);
+    };
+  });
+  return callback;
 }
 
 // Run tests.
 if (module === require.main) {
-    asyncStack(tests)();
+  asyncStack(tests)();
 }
