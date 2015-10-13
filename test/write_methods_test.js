894fc2ba047249895b5d4d6d016b3c1c49ab1150
Kris Walker <kris@kixx.name>
2015-10-13 16:25:24 -0400

    write methods is delinted

--- test/write_methods_test.js
+++ test/write_methods_test.js
@@ -1,11 +1,10 @@
-var FS       = require('fs')
+var FS = require('fs');
 
-	, TOOLS    = require('./tools')
-	, FILEPATH = require('../index')
+var TOOLS = require('./tools');
+var FILEPATH = require('../index');
 
-
-exports["#.write() method"] = {
-	setUp: function (done) {
+exports['#.write() method'] = {
+	'setUp': function (done) {
 		FILEPATH.newPath('/tmp/test-write-file.txt').remove();
 		FILEPATH.newPath('/tmp/test-write-file-sync.txt').remove();
 		FILEPATH.newPath('/tmp/new-dir/test-write-file.txt').remove();
@@ -14,9 +13,9 @@ exports["#.write() method"] = {
 		return done();
 	},
 
-	"writes plain text files by default": function (test) {
+	'writes plain text files by default': function (test) {
 		test.expect(3);
-		var path = FILEPATH.newPath('/tmp/test-write-file.txt')
+		var path = FILEPATH.newPath('/tmp/test-write-file.txt');
 		test.strictEqual(path.exists(), false, 'path does not exist yet');
 
 		function testPlainText(path) {
@@ -26,26 +25,26 @@ exports["#.write() method"] = {
 			return;
 		}
 
-		path.write("foo=bar")
+		path.write('foo=bar')
 			.then(testPlainText)
-			.then(test.done, test.done)
+			.then(test.done, test.done);
 	},
 
-	"writes plain text files synchronously": function (test) {
+	'writes plain text files synchronously': function (test) {
 		test.expect(3);
-		var path = FILEPATH.newPath('/tmp/test-write-file-sync.txt')
+		var path = FILEPATH.newPath('/tmp/test-write-file-sync.txt');
 		test.strictEqual(path.exists(), false, 'path does not exist yet');
 
-		path.write("foo=bar", {sync: true});
+		path.write('foo=bar', {sync: true});
 		test.ok(path.exists(), 'path exists');
 		var rv = FS.readFileSync(path.toString(), 'utf8');
 		test.equal(rv, 'foo=bar');
 		test.done();
 	},
 
-	"creates directories if needed": function (test) {
+	'creates directories if needed': function (test) {
 		test.expect(3);
-		var path = FILEPATH.newPath('/tmp/new-dir/test-write-file.txt')
+		var path = FILEPATH.newPath('/tmp/new-dir/test-write-file.txt');
 		test.strictEqual(path.exists(), false, 'path does not exist yet');
 
 		function testPlainText() {
@@ -55,25 +54,25 @@ exports["#.write() method"] = {
 			return;
 		}
 
-		path.write("deep foo=bar")
+		path.write('deep foo=bar')
 			.then(testPlainText)
-			.then(test.done, test.done)
+			.then(test.done, test.done);
 	},
 
-	"creates directories if needed": function (test) {
+	'creates directories if needed (sync)': function (test) {
 		test.expect(3);
-		var path = FILEPATH.newPath('/tmp/new-dir/test-write-file-sync.txt')
+		var path = FILEPATH.newPath('/tmp/new-dir/test-write-file-sync.txt');
 		test.strictEqual(path.exists(), false, 'path does not exist yet');
 
-		path.write("deep foo=bar", {sync: true});
+		path.write('deep foo=bar', {sync: true});
 		test.ok(path.exists(), 'path exists');
 		var rv = FS.readFileSync(path.toString(), 'utf8');
 		test.equal(rv, 'deep foo=bar');
 		test.done();
 	},
 
-	"throws when a path is a directory": function (test) {
-		var path = FILEPATH.newPath(__dirname)
+	'throws when a path is a directory': function (test) {
+		var path = FILEPATH.newPath(__dirname);
 
 		function skip(rv) {
 			console.log(rv);
@@ -84,44 +83,43 @@ exports["#.write() method"] = {
 		test.expect(3);
 		function onFailure(err) {
 			test.equal(err.name, 'FilePathError', 'FilePathError');
-			test.equal(err.code, "PATH_IS_DIRECTORY", 'FilePathError code');
-			test.equal(err.message, "Cannot write to '"+ __dirname +"'; it is a directory.", 'FilePathError message');
+			test.equal(err.code, 'PATH_IS_DIRECTORY', 'FilePathError code');
+			test.ok(/Cannot write to ([.]+) it is a directory/.test(err.message), 'FilePathError message');
 			return;
 		}
 
 		path.write()
 			.then(skip, onFailure)
-			.then(test.done, test.done)
+			.then(test.done, test.done);
 	},
 
-	"throws when a path is a directory": function (test) {
-		var path = FILEPATH.newPath(__dirname)
+	'throws when a path is a directory (sync)': function (test) {
+		var path = FILEPATH.newPath(__dirname);
 
 		test.expect(3);
 		try {
 			path.write('', {sync: true});
 		} catch (err) {
 			test.equal(err.name, 'FilePathError', 'FilePathError');
-			test.equal(err.code, "PATH_IS_DIRECTORY", 'FilePathError code');
-			test.equal(err.message, "Cannot write to '"+ __dirname +"'; it is a directory.", 'FilePathError message');
+			test.equal(err.code, 'PATH_IS_DIRECTORY', 'FilePathError code');
+			test.ok(/Cannot write to ([.]+) it is a directory/.test(err.message), 'FilePathError message');
 		}
 
 		test.done();
 	}
 };
 
-exports["#copy() method"] = {
-	setUp: function (done) {
+exports['#copy() method'] = {
+	'setUp': function (done) {
 		FILEPATH.newPath('/tmp/copied-test.ini').remove();
 		FILEPATH.newPath('/tmp/copied-test-sync.ini').remove();
 		return done();
 	},
 
-
-	"copies to target path": function (test) {
+	'copies to target path': function (test) {
 		test.expect(4);
-		var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
-			, target = FILEPATH.root().append('tmp', 'copied-test.ini')
+		var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini');
+		var target = FILEPATH.root().append('tmp', 'copied-test.ini');
 
 		test.strictEqual(target.exists(), false, 'path does not exist yet');
 
@@ -141,10 +139,10 @@ exports["#copy() method"] = {
 			.catch(test.done);
 	},
 
-	"copies to target path synchronously": function (test) {
+	'copies to target path synchronously': function (test) {
 		test.expect(4);
-		var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini')
-			, target = FILEPATH.root().append('tmp', 'copied-test-sync.ini')
+		var path = FILEPATH.newPath(__dirname, 'fixtures', 'test.ini');
+		var target = FILEPATH.root().append('tmp', 'copied-test-sync.ini');
 
 		test.strictEqual(target.exists(), false, 'path does not exist yet');
 
