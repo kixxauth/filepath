0b77dc63167992449523e40b8f365d09fa819152
Kris Walker <kris@kixx.name>
2015-10-13 14:03:32 -0400

    delint tools/tools.js

--- test/tools.js
+++ test/tools.js
@@ -1,14 +1,13 @@
-var PATH = require('path')
-
+var PATH = require('path');
 
 exports.platformString = function (str) {
-	var parts = str.split('/')
-		, path  = parts.join(PATH.sep)
+	var parts = str.split('/');
+	var path = parts.join(PATH.sep);
 	return PATH.resolve(path);
 };
 
 exports.platformLines = function (str) {
-	var parts = str.split('\n')
+	var parts = str.split('\n');
 	return parts.join(exports.platformLineEnding());
 };
 
