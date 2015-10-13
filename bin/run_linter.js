16f6012372c8749d0692f9ebb807b681070492f8
Kris Walker <kris@kixx.name>
2015-10-13 11:07:21 -0400

    adds lint script to package.json

---
+++ bin/run_linter.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+'use strict';
+
+var
+CP       = require('child_process'),
+FilePath = require('../').FilePath,
+Promise  = require('bluebird');
+
+exports.main = function () {
+  console.log(' ### running lint_runner ###');
+
+  var
+  execFile = process.platform === 'win32' ? 'xo.cmd' : 'xo';
+  return new Promise(function (resolve, reject) {
+    var
+    exec = FilePath.create().append('node_modules', '.bin', execFile),
+    proc = CP.spawn(exec.toString());
+    proc.stdout.pipe(process.stdout);
+    proc.stderr.pipe(process.stderr);
+    proc.on('close', function (code) {
+      if (code === 0) {
+        return resolve(0);
+      }
+      return reject(new Error('Failed linter'));
+    });
+  });
+};
+
+
+if (require.main === module) {
+  exports.main()
+    .then(function () {
+      console.log('lint_runner succeeded :)');
+    })
+    .catch(function (err) {
+      console.error('lint_runner failed:');
+      console.error(err.stack || err.message || err);
+    });
+}
