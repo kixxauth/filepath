ebbc6c8df4ae3039859a691cfb1f0e40514edf86
Kristoffer Walker <kris@kixx.name>
2015-10-22 05:55:33 -0400

    Better test runner.
    	modified:   bin/run_tests.js

--- bin/run_tests.js
+++ bin/run_tests.js
@@ -1,29 +1,65 @@
-var NFS = require('fs');
-var NPATH = require('path');
+#!/usr/bin/env node
+'use strict';
 
-var NODEUNIT = require('nodeunit');
+var Promise = require('bluebird');
+var filepath = require('../');
+var nodeunit = require('nodeunit');
 
-var testPath = NPATH.resolve(process.argv[2]);
-var fileMatcher = /test\.js$/;
-var files;
+var FILEMATCHER = /test\.js$/;
+
+// args.path - A FilePath instance
+exports.main = function (args) {
+	console.log(' ### running tests ###');
+	var path = args.path;
+	var files;
+
+	if (path.isDirectory()) {
+		files = readTree(path);
+	} else {
+		files = [path.toString()];
+	}
+
+	return runNodeunit(files);
+};
+
+// If this is the main run module, execute automatically.
+if (require.main === module) {
+	exports.main({path: filepath.create().resolve(process.argv[2])})
+		.then(function () {
+			console.log('run_tests succeeded :)');
+		})
+		.catch(function (err) {
+			console.error('run_tests failed:');
+			console.error(err.stack || err.message || err);
+		});
+}
+
+//
+// Utilities
+//
+
+function runNodeunit(files) {
+	return new Promise(function (resolve, reject) {
+		nodeunit.reporters.default.run(files, null, function (err) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(true);
+			}
+		});
+	});
+}
 
 function readTree(dir) {
 	var collection = [];
-	var list = NFS.readdirSync(dir);
 
-	list.forEach(function (item) {
-		var filepath = NPATH.join(dir, item);
-		var stats = NFS.statSync(filepath);
-
-		if (stats.isDirectory()) {
-			collection = collection.concat(readTree(filepath));
-		} else if (stats.isFile() && fileMatcher.test(filepath)) {
-			collection.push(NPATH.relative(process.cwd(), filepath));
+	dir.list().forEach(function (path) {
+		if (path.isDirectory()) {
+			collection = collection.concat(readTree(path));
+		} else if (path.isFile() && FILEMATCHER.test(path.toString())) {
+			collection.push(path.toString());
 		}
 	});
 
 	return collection;
 }
-
-files = readTree(testPath);
-NODEUNIT.reporters.default.run(files);
